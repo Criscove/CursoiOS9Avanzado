3c0019e40f2288b5e930df343973e64d2ed71393
Cristian Contreras Velásquez <ccontrerasv@gmail.com>
2016-02-29 13:24:23 -0300

    Comit inicial del proyecto T1E01

---
+++ T1E01/T1E01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T1E01
+//
+//  Created by Cristian Contreras Velásquez on 29-02-16.
+//  Copyright © 2016 Cristian Contreras Velásquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
