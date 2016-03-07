3307ac06914fd4d42b82f812f6d779e7ba8ab43e
Cristian Contreras Velásquez <ccontrerasv@gmail.com>
2016-03-07 18:28:17 -0300

    comit Ejercicio 1 Tema 3

---
+++ T2E1/T2E1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T2E1
+//
+//  Created by Cristian Contreras Velásquez on 07-03-16.
+//  Copyright © 2016 Cristian Contreras Velásquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
